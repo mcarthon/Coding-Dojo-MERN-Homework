a8dcad3ea27f90e6e9f228ffee6606884963d2ad
Mark Carthon <macarthoniii@gmail.com>
2023-06-30 23:22:42 -0400

    still need to understand why I should initialize any default parameters in the super(). I believe it is because those values, for the child class, should be initialized in the constructor and the super is just taking care of the leftovers from the parent class that were not addressed by the constructor

--- ninja.js
+++ ninja.js
@@ -42,3 +42,39 @@ const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 
 ninja1.showStats();
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class Sensei extends Ninja {
+
+    constructor(name = "Strong Ling", health = 200, speed = 10, strength = 10, wisdom = 10) {
+
+        super(name, health, speed, strength);
+
+        this.name = name;
+
+        this.health = health;
+
+        this.speed = speed;
+
+        this.strength = strength;
+
+        this.wisdom = wisdom;
+
+    }            
+
+    speakWisdom() {
+
+        super.drinkSnake();
+
+        console.log("When I drink the snake, I feel more wise.");
+
+    }
+
+}
+
+const superSensei = new Sensei("Master Splinter");
+
+superSensei.speakWisdom();
+
+superSensei.showStats();
