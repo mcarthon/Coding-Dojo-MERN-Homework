d4f811d7f3b365b5c7fd4dd5c408add347ce6c29
Mark Carthon <macarthoniii@gmail.com>
2023-07-01 15:50:24 -0400

    finished prop-it-up

---
+++ prop-it-up/src/App.js
@@ -0,0 +1,20 @@
+import PersonCard from './components/PersonCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="root">
+      
+        <PersonCard firstName="Jane" lastName="Doe" age="45" hairColor="Black"></PersonCard>
+
+        <PersonCard firstName="John" lastName="Smith" age="88" hairColor="Brown"></PersonCard>
+
+        <PersonCard firstName="Millard" lastName="Fillmore" age="50" hairColor="Brown"></PersonCard>
+
+        <PersonCard firstName="Maria" lastName="Smith" age="62" hairColor="Brown"></PersonCard>
+
+    </div>
+  );
+}
+
+export default App;
