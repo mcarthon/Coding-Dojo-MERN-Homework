46f206c58ff3fed362a763544f8d6192ddfbdba3
Mark Carthon <macarthoniii@gmail.com>
2023-07-01 17:16:53 -0400

    finished 'putting it all together'

--- prop-it-up/src/App.js
+++ prop-it-up/src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="root">
       
-        <PersonCard firstName="Jane" lastName="Doe" age="45" hairColor="Black"></PersonCard>
+        <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="Black"></PersonCard>
 
-        <PersonCard firstName="John" lastName="Smith" age="88" hairColor="Brown"></PersonCard>
+        <PersonCard firstName="John" lastName="Smith" age={88} hairColor="Brown"></PersonCard>
 
-        <PersonCard firstName="Millard" lastName="Fillmore" age="50" hairColor="Brown"></PersonCard>
+        <PersonCard firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown"></PersonCard>
 
-        <PersonCard firstName="Maria" lastName="Smith" age="62" hairColor="Brown"></PersonCard>
+        <PersonCard firstName="Maria" lastName="Smith" age={62} hairColor="Brown"></PersonCard>
 
     </div>
   );
--- prop-it-up/src/components/PersonCard.js
+++ prop-it-up/src/components/PersonCard.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
+
+    const [state, setState] = useState({
+
+        age: props.age
+
+    });
+
+    const handleClick = () => {
+
+        setState({
+
+            age: state.age + 1
+
+        });
+
+    }
 
     return (
 
@@ -8,9 +24,11 @@ const PersonCard = (props) => {
 
             <h1> { props.lastName }, { props.firstName } </h1>
 
-            <p> Age: { props.age } </p>
+            <p> Age: { state.age } </p>
 
             <p> Age: { props.hairColor } </p>
+
+            <button onClick={ handleClick }> Birthday Button for { props.firstName } { props.lastName } </button>
 
         </div>
 
