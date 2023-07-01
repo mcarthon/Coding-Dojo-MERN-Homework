8bcc79bc4cdbecf72798ccb9f702cab7f9ee2d16
Mark Carthon <macarthoniii@gmail.com>
2023-07-01 15:04:54 -0400

    finished first React with JSX. just had to disable firewall and VPN to get things running. so excited for what is next

---
+++ my-new-app/src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="root">
+      
+    <>
+    
+        <h1> Hello Dojo! </h1>
+
+        <h2> Things I need to do: </h2>
+
+        <li> Learn React </li>
+
+        <li> Climb Mount Everest </li>
+
+        <li> Run a Marathon </li>
+
+        <li> Feed the Dogs </li>
+
+    </>
+
+    </div>
+  );
+}
+
+export default App;
